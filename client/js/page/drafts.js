a7310b6865bf95eff268876e7c8eef00af123bbf
cpunt <christopherpunt@hotmail.com>
2020-11-01 15:47:54 +0000

    Refactor drafts.js

--- client/js/page/drafts.js
+++ client/js/page/drafts.js
@@ -1,31 +1,46 @@
 window.onload = function() {
-  const request = getRequest();
-  load(request);
+  load(getRequest());
 }
 
-function load(request) {
-  const xmlhttp = new XMLHttpRequest();
-  xmlhttp.open('GET', `/articles/index.php/?loadPage=${JSON.stringify(request)}`, true);
+async function load(data) {
+  const request = await fetch(`/articles/index.php/?loadPage=${JSON.stringify(data)}`, {
+    method: 'GET'
+  });
+  const response = JSON.parse(await request.text());
+  window.history.pushState('page', '', `/articles/drafts/page/${response['pagination']['page']}`);
 
-  xmlhttp.onreadystatechange = function() {
-    if(xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-      const res = JSON.parse(xmlhttp.responseText);
-      const pagination = res['pagination'];
-      const drafts = res['items'];
-      const url = `/articles/drafts/page/${pagination['page']}`
-      window.history.pushState('page', '', url)
+  if(!response['validRequest']) {
+    window.location.href = `/articles`;
+  } else {
+    displayDrafts(response['items']);
+    displayPaginationDiv(response['pagination']);
+  }
+}
 
-      if(res['validRequest']) {
-        displayDrafts(drafts);
+async function deleteDraft(ref) {
+  if (confirm('Are you sure you want to delete this draft? This cannot be undone.')) {
+
+    const data = {
+      type: 'delete',
+      ref: ref
+    };
+    const request = await fetch('/articles/index.php', {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+      },
+      body: `edit=${JSON.stringify(data)}`
+    });
+    const response = JSON.parse(await request.text());
+
+    if(response['valid']) {
+      if(response['deleted']) {
+        location.reload();
       } else {
-        window.location.href = `/articles`;
+        throw new Error('Error deleting');
       }
-
-      displayPaginationDiv(pagination);
     }
   }
-
-  xmlhttp.send();
 }
 
 function displayDrafts(drafts) {
@@ -49,37 +64,7 @@ function displayDrafts(drafts) {
           <img class='d-inline float-right mr-3 icon' src='/articles/client/assets/delete.svg' width='24' height='24' onclick="deleteDraft('${drafts[i].articleref}')">
         </div>
       </div>
-      `
+      `;
     }
-  }
-}
-
-function deleteDraft(ref) {
-  if (confirm('Are you sure you want to delete this draft? This cannot be undone.')) {
-
-    const request = {
-      type: 'delete',
-      ref: ref
-    };
-
-    const xmlhttp = new XMLHttpRequest();
-    xmlhttp.open('DELETE', '/articles/index.php', true);
-    xmlhttp.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
-
-    xmlhttp.onreadystatechange = function() {
-      if(xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-        const res = JSON.parse(xmlhttp.responseText);
-
-        if(res['valid']) {
-          if(res['deleted']) {
-            location.reload();
-          } else {
-            throw new Error('Error deleting');
-          }
-        }
-      }
-    }
-
-    xmlhttp.send(`edit=${JSON.stringify(request)}`);
   }
 }
