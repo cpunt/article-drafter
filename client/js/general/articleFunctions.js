85e2c52c393f6a49f5f6ddf87d882849a3e78e95
cpunt <christopherpunt@hotmail.com>
2020-11-03 17:26:54 +0000

    Refactor articleFunctions.js

--- client/js/general/articleFunctions.js
+++ client/js/general/articleFunctions.js
@@ -1,28 +1,25 @@
-function deleteArticle(articleref) {
+async function deleteArticle(articleRef) {
   if (confirm('Are you sure you want to delete this article? This cannot be undone.')) {
-    const editData = {
+    const data = {
       type: 'delete',
-      ref: articleref
-    };
-
-    const xmlhttp = new XMLHttpRequest();
-    xmlhttp.open('DELETE', '/articles/index.php', true);
-    xmlhttp.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
-
-    xmlhttp.onreadystatechange = function() {
-      if(xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-        const res = JSON.parse(xmlhttp.responseText);
-
-        if(res['valid']) {
-          if(res['deleted']) {
-            location.reload();
-          } else {
-            throw new Error('Error deleting');
-          }
-        }
-      }
+      ref: articleRef
     }
 
-    xmlhttp.send(`edit=${JSON.stringify(editData)}`);
+    const request = await fetch('/articles/index.php', {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+      },
+      body: `edit=${encodeURIComponent(JSON.stringify(data))}`
+    });
+    const response = JSON.parse(await request.text());
+
+    if(response['valid']) {
+      if(response['deleted']) {
+        location.reload();
+      } else {
+        throw new Error('Error deleting');
+      }
+    }
   }
 }
